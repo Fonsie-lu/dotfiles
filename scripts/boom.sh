d1b90133e050f7368eaae347ca36d7def1524baa
Beat Weber <beat.weber.86@gmail.com>
2019-12-06 10:03:30 +0100

    +scripts

---
+++ scripts/boom.sh
@@ -0,0 +1,2 @@
+echo -e 'power on\nconnect C0:28:8D:FE:78:00 \nquit' | bluetoothctl
+
---
+++ scripts/install.sh
@@ -0,0 +1,1 @@
+sudo pacman -S vim neovim ranger thunar zsh-syntax-highlighting tmux zip unzip rofi rtorrent tlp rxvt-unicode pfetch polybar nitrogen feh picom
