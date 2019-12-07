ac3e13d6314322245c20778024067c8e53a8d759
Beat Weber <beat.weber.86@gmail.com>
2019-12-07 09:07:00 +0100

    update dotfiles

---
+++ scripts/pull.sh
@@ -0,0 +1,10 @@
+cd ~/Downloads/dotfiles/
+git pull
+
+cp -f ~/Downloads/dotfiles/.zshrc ~/.zshrc 
+cp -f ~/Downloads/dotfiles/.Xresources ~/.Xresources 
+cp -f -r ~/Downloads/dotfiles/.config/i3/* ~/.config/i3/  
+cp -f -r ~/Downloads/dotfiles/.config/polybar/* ~/.config/polybar/  
+cp -f -r ~/Downloads/dotfiles/.config/nvim/* ~/.config/nvim/
+cp -f -r ~/Downloads/dotfiles/Pictures/* ~/Pictures/
+cp -f -r ~/Downloads/dotfiles/scripts/* ~/scripts/
--- scripts/push.sh
+++ scripts/push.sh
@@ -4,10 +4,11 @@ cp -f -r ~/.config/i3/* ~/Downloads/dotfiles/.config/i3/
 cp -f -r ~/.config/polybar/* ~/Downloads/dotfiles/.config/polybar/
 cp -f -r ~/.config/nvim/* ~/Downloads/dotfiles/.config/nvim/
 cp -f -r ~/Pictures/* ~/Downloads/dotfiles/Pictures/
+cp -f -r ~/scripts/* ~/Documents/dotfiles/scripts/
 
 cd ~/Downloads/dotfiles/
 git pull
 git add --all
 git commit -m "update dotfiles"
-git push master origin
+git push 
 echo "finished"
