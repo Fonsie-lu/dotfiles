59e595b04ade8694e66ed620fc5669c7f9eaa1ad
Beat Weber <beat.weber.86@gmail.com>
2019-12-15 11:23:16 +0100

    pfetch

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 sudo pacman -S vim neovim ranger thunar zip unzip rofi rtorrent tlp rxvt-unicode nitrogen feh picom bluez-utils pulseaudio-bluetooth git gtk-engine murrine qutebrowser urxvt-perls code godot  
-yay -S polybar neovim-plug ttf-nerd-fonts zsh-syntax-highlighting powerline-font ttf-font-awesome vim-gruvbox-git zsh-theme-powerlevel10k-git
+yay -S polybar neovim-plug ttf-nerd-fonts zsh-syntax-highlighting powerline-font ttf-font-awesome vim-gruvbox-git zsh-theme-powerlevel10k-git pfetch
 sudo cp backlight.rules /etc/udev/rules.d/
