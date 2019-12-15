fc3c428e8f21115ad4fcd3cbe618a8aff2e25df8
Beat Weber <beat.weber.86@gmail.com>
2019-12-15 11:21:47 +0100

    added scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
-sudo pacman -S vim neovim ranger thunar tmux zip unzip rofi rtorrent tlp rxvt-unicode nitrogen feh picom 
-yay -S polybar neovim-plug ttf-nerd-fonts zsh-syntax-highlighting  
-
+sudo pacman -S vim neovim ranger thunar zip unzip rofi rtorrent tlp rxvt-unicode nitrogen feh picom bluez-utils pulseaudio-bluetooth git gtk-engine murrine qutebrowser urxvt-perls code godot  
+yay -S polybar neovim-plug ttf-nerd-fonts zsh-syntax-highlighting powerline-font ttf-font-awesome vim-gruvbox-git zsh-theme-powerlevel10k-git
+sudo cp backlight.rules /etc/udev/rules.d/
