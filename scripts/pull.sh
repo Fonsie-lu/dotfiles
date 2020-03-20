cc18b1fd73ff19ce769b43b9b913396b12dbfa90
Beat Weber <beat.weber.86@gmail.com>
2020-03-20 18:32:59 +0100

    update dotfiles

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -5,6 +5,7 @@ cp -f ~/Downloads/dotfiles/.zshrc ~/.zshrc
 cp -f ~/Downloads/dotfiles/.Xresources ~/.Xresources 
 cp -f -r ~/Downloads/dotfiles/.config/i3/* ~/.config/i3/  
 cp -f -r ~/Downloads/dotfiles/.config/polybar/* ~/.config/polybar/  
+cp -f -r ~/Downloads/dotfiles/.config/polybar/* ~/.config/ranger/  
 cp -f -r ~/Downloads/dotfiles/.config/nvim/* ~/.config/nvim/
 cp -f -r ~/Downloads/dotfiles/Pictures/* ~/Pictures/
 cp -f -r ~/Downloads/dotfiles/scripts/* ~/scripts/
