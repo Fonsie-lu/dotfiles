bc8f88d7641d737823da499e8345de30d93f1fea
Beat Weber Notebook <beat.weber.86@gmail.com>
2023-02-09 19:09:01 +0100

    update

--- hyprland.sh
+++ hyprland.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 paru -S --noconfirm hyprland-nvidia-git xdg-desktop-portal-hyprland-git wlogout waybar-hyprland-git swaync swaybg-git grimblast-git grim slurp swaylock-effects-git
+paru -S --noconfirm catppuccin-gtk-theme-mocha catppuccin-kde-theme-git catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git papirus-folders-catppuccin-git
