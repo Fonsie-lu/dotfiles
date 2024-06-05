3d717d32125ec5492db8b5826d477836c5424f03
Fonsie-lu <58588383+Fonsie-lu@users.noreply.github.com>
2024-06-05 21:49:11 +0200

    Update install-void.sh

--- install-void.sh
+++ install-void.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
 ## Packages
-sudo xbps-install polkit NetworkManager Waybar alacritty alsa-firmware alsa-pipewire android-file-transfer-linux bat bluetuith bluez btop chromium courier-unicode curl dunst elogind fail2ban fastfetch ffmpeg fzf gcc git gnome-disk-utility grim gtk-engine-murrine gtk2-engines i2c-tools icu imv kvantum libbluetooth libreoffice linux-mainline lm_sensors lsd mesa-dri mpv mtpfs nemo neovim nerd-fonts network-manager-applet nftables nodejs ntfs-3g nwg-look opendoas pa-applet pamixer papirus-folders pavucontrol pipewire qbittorrent qt5-styleplugins qt5ct ranger seatd slurp starship tldr unicode-emoji unzip vscode wget wl-clipboard wlogout wofi xdg-user-dirs xorg-fonts youtube-dl yt-dlp zsh gvfs-mtp gstreamer-vaapi mesa-vaapi fd greetd tuigreet
-
-## Enable Services
-sudo ln -s /etc/sv/seatd/ /var/service/
-sudo ln -s /etc/sv/nftables// /var/service/
-sudo ln -s /etc/sv/dbus/ /var/service/
-sudo ln -s /etc/sv/fail2ban/ /var/service/
-sudo ln -s /etc/sv/polkitd/ /var/service/
-sudo ln -s /etc/sv/greetd/ /var/service/
-
-sudo usermod -aG fonsie _seatd
-sudo cp .config/config.toml /etc/greetd/
-sudo mkdir -p /var/cache/tuigreet/
-sudo chown _greeter /var/cache/tuigreet
+sudo xbps-install polkit NetworkManager Waybar alacritty alsa-firmware alsa-pipewire android-file-transfer-linux bat bluetuith bluez btop chromium courier-unicode curl dunst elogind fail2ban fastfetch ffmpeg fzf gcc git gnome-disk-utility grim gtk-engine-murrine gtk2-engines i2c-tools icu imv kvantum libbluetooth libreoffice linux-mainline lm_sensors lsd mesa-dri mpv mtpfs nemo neovim nerd-fonts network-manager-applet nftables nodejs ntfs-3g nwg-look opendoas pa-applet pamixer papirus-folders pavucontrol pipewire qbittorrent qt5-styleplugins qt5ct ranger seatd slurp starship tldr unicode-emoji unzip vscode wget wl-clipboard wlogout wofi xdg-user-dirs xorg-fonts youtube-dl yt-dlp zsh gvfs-mtp gstreamer-vaapi mesa-vaapi fd greetd tuigreet xorg-server-xwayland
 
 ## Build Hyprland
 mkdir -p ~/.local/pkgs/
@@ -32,8 +19,11 @@ cat common/shlibs >>../void-packages/common/shlibs
 cp -r srcpkgs/* ../void-packages/srcpkgs
 
 cd ../void-packages
-./xbps-src pkg hyprland hyprpaper xdg-desktop-portal-hyprland hyprland-protocols
-sudo xbps-install -R hostdir/binpkgs hyprland hyprpaper xdg-desktop-portal-hyprland hyprland-protocols
+./xbps-src pkg hyprland
+./xbps-src pkg xdg-desktop-portal-hyprland
+./xbps-src pkg hyprpaper
+./xbps-src pkg hyprland-protocols
+sudo xbps-install -R hostdir/binpkgs hyprland xdg-desktop-portal-hyprland hyprland-protocols hyprpaper
 cd
 
 ## Use Doas
@@ -42,9 +32,9 @@ sudo rm /usr/sudo
 sudo ln -s $(which doas) /user/bin/sudo
 
 ## Pipewire
-mkdir -p /etc/pipewire/pipewire.conf.d
-ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
-ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
+sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
 ## NPM Modules for work
 mkdir -p ~/doc/sn-sync/
@@ -67,9 +57,26 @@ cd
 ## misc
 sudo sensors-detect --auto
 xdg-user-dirs-update
-sudo nvim /etc/default/grub
 
-#Set git config
 git config --global user.email "beat.weber.86@gmail.com"
 git config --global user.name "Beat Weber Notebook"
 git config --global credential.helper store
+
+sudo usermod -aG fonsie _seatd
+sudo cp ~dl/dotfiles/.config/config.toml /etc/greetd/
+sudo mkdir -p /var/cache/tuigreet/
+sudo chown _greeter /var/cache/tuigreet
+
+exit
+
+sudo nvim /etc/default/grub
+
+## Enable Services
+sudo ln -s /etc/sv/seatd/ /var/service/
+sudo ln -s /etc/sv/nftables// /var/service/
+sudo ln -s /etc/sv/dbus/ /var/service/
+sudo ln -s /etc/sv/fail2ban/ /var/service/
+sudo ln -s /etc/sv/polkitd/ /var/service/
+sudo ln -s /etc/sv/greetd/ /var/service/
+
+
