eba1c5e4da614fda6ccc0699e3be638251d11d92
fonsie <fonsie@void.home>
2024-06-05 19:31:36 +0200

    update

--- install-void.sh
+++ install-void.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 ## Packages
-sudo xbps-install polkit NetworkManager Waybar alacritty alsa-firmware alsa-pipewire android-file-transfer-linux bat bluetuith bluez btop chromium courier-unicode curl dunst elogind emptty fail2ban fastfetch ffmpeg fzf gcc git gnome-disk-utility grim gtk-engine-murrine gtk2-engines i2c-tools icu imv kvantum libbluetooth libreoffice linux-mainline lm_sensors lsd mesa-dri mpv mtpfs nemo neovim nerd-fonts network-manager-applet nftables nodejs ntfs-3g nwg-look opendoas pa-applet pamixer papirus-folders pavucontrol pipewire qbittorrent qt5-styleplugins qt5ct ranger seatd slurp starship tldr unicode-emoji unzip vscode wget wl-clipboard wlogout wofi xdg-user-dirs xorg-fonts youtube-dl yt-dlp zsh gvfs-mtp gstreamer-vaapi mesa-vaapi fd
+sudo xbps-install polkit NetworkManager Waybar alacritty alsa-firmware alsa-pipewire android-file-transfer-linux bat bluetuith bluez btop chromium courier-unicode curl dunst elogind fail2ban fastfetch ffmpeg fzf gcc git gnome-disk-utility grim gtk-engine-murrine gtk2-engines i2c-tools icu imv kvantum libbluetooth libreoffice linux-mainline lm_sensors lsd mesa-dri mpv mtpfs nemo neovim nerd-fonts network-manager-applet nftables nodejs ntfs-3g nwg-look opendoas pa-applet pamixer papirus-folders pavucontrol pipewire qbittorrent qt5-styleplugins qt5ct ranger seatd slurp starship tldr unicode-emoji unzip vscode wget wl-clipboard wlogout wofi xdg-user-dirs xorg-fonts youtube-dl yt-dlp zsh gvfs-mtp gstreamer-vaapi mesa-vaapi fd greetd tuigreet
 
 ## Enable Services
 sudo ln -s /etc/sv/seatd/ /var/service/
-sudo ln -s /etc/sv/elogind// /var/service/
 sudo ln -s /etc/sv/nftables// /var/service/
 sudo ln -s /etc/sv/dbus/ /var/service/
 sudo ln -s /etc/sv/fail2ban/ /var/service/
 sudo ln -s /etc/sv/polkitd/ /var/service/
-sudo ln -s /etc/sv/emptty/ /var/service/
+sudo ln -s /etc/sv/greetd/ /var/service/
+sudo ln -s /etc/sv/dmeventd/ /var/service/
 
 sudo usermod -aG fonsie _seatd
+sudo cp .config/config.toml /etc/greetd/
+sudo mkdir -p /var/cache/tuigreet/
+sudo chown _greeter /var/cache/tuigreet
 
 ## Build Hyprland
 mkdir -p ~/.local/pkgs/
